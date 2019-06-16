ff70633bf09b883cdeead94c79d68aaefde68b3b
Lloyd Francis <theapologist@Lloyds-MacBook-Pro.local>
2019-06-17 00:18:01 +0530

    code formatting for consistency, reusing constant

--- src/typer.js
+++ src/typer.js
@@ -1,10 +1,10 @@
-const { EMPTY_STRING, EMPTY_ARRAY, EMPTY_OBJECT } = require('./constants')
+const { EMPTY_STRING, EMPTY_ARRAY, EMPTY_OBJECT, COLON } = require('./constants')
 /**
  * This function will return the key and it's default value depending on the type
  * @param {string} key 
  */
 export function getDefaultValue(key, types = null) {
-  const column = key.indexOf(":")
+  const column = key.indexOf(COLON)
   if (column < 0) return [key, null]
   return [key.slice(0, column), defaultValueForType(key.slice(column + 1).trim(), types)]
 }
@@ -16,9 +16,9 @@ export function getDefaultValue(key, types = null) {
 function defaultValueForType(type, types = null) {
   if (types && types[type]) return types[type]
   switch (type) {
-    case "String": return EMPTY_STRING
-    case "Array": return EMPTY_ARRAY
-    case "Object": return EMPTY_OBJECT
+    case 'String': return EMPTY_STRING
+    case 'Array': return EMPTY_ARRAY
+    case 'Object': return EMPTY_OBJECT
     default: return null
   }
 }
