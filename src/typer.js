bce16cd9ada7efa3ec88a60b5218437a3dcc1b24
Lloyd Francis <theapologist@Lloyds-MacBook-Pro.local>
2019-06-16 20:50:56 +0530

    enabling schema cacheing to define nested schemas

--- src/typer.js
+++ src/typer.js
@@ -2,17 +2,18 @@
  * This function will return the key and it's default value depending on the type
  * @param {string} key 
  */
-export function getDefaultValue(key) {
+export function getDefaultValue(key, types = null) {
   const column = key.indexOf(":")
   if (column < 0) return [key, null]
-  return [key.slice(0, column), defaultValueForType(key.slice(column + 1).trim())]
+  return [key.slice(0, column), defaultValueForType(key.slice(column + 1).trim(), types)]
 }
 
 /**
  * This function will return the default value for the type provided for the property
  * @param {string} type 
  */
-function defaultValueForType(type) {
+function defaultValueForType(type, types = null) {
+  if (types && types[type]) return types[type]
   switch (type) {
     case "String": return ""
     case "Array": return []
