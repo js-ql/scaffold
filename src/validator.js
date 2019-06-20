ca8c4e98071a5af9459373b4797d8c13c3904e49
Lloyd Francis <lloydaf@gmail.com>
2019-06-21 01:21:00 +0530

    Adding more test cases for validator, improving the validator function, fixing bugs

--- src/validator.js
+++ src/validator.js
@@ -1,20 +1,37 @@
-const { OPENING_CURLY, CLOSING_CURLY } = require('./constants')
+const { OPENING_CURLY, CLOSING_CURLY, NEWLINE, SPACE, EMPTY_STRING } = require('./constants')
 /**
  * Determines if the query string is valid
  * @param {string} qlString The Query String to check validity for
  * @returns {boolean} True or False, depending on the validity of the query string
  */
 const isValidQl = (qlString) => {
-  if(!qlString) return false
+  if (!qlString || typeof qlString !== 'string') return false
   let k = 0
+  let lastChar = null
+  let curlyFlag = false
   for (let i = 0; i < qlString.length; i++) {
-    if (qlString[i] === OPENING_CURLY)
-      k++
-    else if (qlString[i] === CLOSING_CURLY)
-      k--
+    const char = qlString[i]
+    if (char === OPENING_CURLY || char === CLOSING_CURLY) {
+      curlyFlag = true
+      if (char === OPENING_CURLY) {
+        if(lastChar === OPENING_CURLY) return false
+        lastChar = OPENING_CURLY
+        k++
+      }
+      else if (char === CLOSING_CURLY) {
+        if (lastChar === OPENING_CURLY) {
+          return false
+        }
+        k--
+      }
+    }
+    else if (char !== NEWLINE && char !== SPACE) {
+      lastChar = char
+    }
     if (k === -1)
       return false
   }
+  if(!curlyFlag) return false
   return k === 0
 }
 
--- tests/validator.spec.js
+++ tests/validator.spec.js
@@ -1,13 +1,91 @@
-const {isValidQl} = require('../src/validator')
+const { isValidQl } = require('../src/validator')
 
-describe('Validator Tests', () => {
+describe('basic tests for isValidQl function:', () => {
   it('should contain a function isValidQl', () => {
     expect(typeof isValidQl).toBe('function')
   })
 
-  it('should handle null/undefined cases', () => {
+  it('should handle null/undefined/empty string cases', () => {
     expect(isValidQl()).toBeFalsy()
     expect(isValidQl(null)).toBeFalsy()
     expect(isValidQl(undefined)).toBeFalsy()
+    expect(isValidQl('')).toBeFalsy()
+  })
+
+  it('should only accept strings', () => {
+    expect(isValidQl(5)).toBeFalsy()
+    expect(isValidQl({})).toBeFalsy()
+    expect(isValidQl([])).toBeFalsy()
+    expect(isValidQl({ name: 'hello' })).toBeFalsy()
+    expect(isValidQl([Symbol])).toBeFalsy()
+    expect(isValidQl('{}')).toBeFalsy()
+  })
+
+  it('should make sure brackets are to be balanced', () => {
+    expect(isValidQl('{name}')).toBeTruthy()
+    expect(isValidQl('{{}{}')).toBeFalsy()
+    expect(isValidQl('}{')).toBeFalsy()
+    expect(isValidQl('{{}}}{')).toBeFalsy()
+    expect(isValidQl(`
+    {
+      name {
+        age
+        }
+      }
+    }
+    `)).toBeFalsy()
+  })
+
+  it('should not accept schemas without keys', () => {
+    expect(isValidQl('{}{}')).toBeFalsy()
+    expect(isValidQl('{name{}}')).toBeFalsy()
+    expect(isValidQl('{{name}}')).toBeFalsy()
+
+  })
+
+  it('should accept multiline strings', () => {
+
+    expect(isValidQl(`
+    {
+    }
+    `)).toBeFalsy()
+    expect(isValidQl(`
+    {
+      name
+      age
+    }
+    `)).toBeTruthy()
+
+    expect(isValidQl(`
+    name
+    `)).toBeFalsy()
+
+    expect(isValidQl(`
+    {
+      name {
+        age
+      }
+    }
+    `)).toBeTruthy()
+
+    expect(isValidQl(`
+    {
+      name {
+        age {
+        }
+      }
+    }
+    `)).toBeFalsy()
+
+    expect(isValidQl(`
+    {
+      name {
+        age {
+          type
+        }
+      }
+    }
+    `)).toBeTruthy()
+
   })
 })
